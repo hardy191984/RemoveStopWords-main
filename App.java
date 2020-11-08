584f0fea60a12cbc29de93df51dabe7c4ce646f9
Hardik Patel <74027203+hardy191984@users.noreply.github.com>
2020-11-08 18:46:44 -0500

    Update App.java

--- App.java
+++ App.java
@@ -8,50 +8,58 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-public class App {
+public class App 
+{
+  public static void main(String[] args) 
+  {
+    try 
+    {
+        // Load the text files(stopwords.txt and Text1.txt)
+        Scanner stopWordsFile = new Scanner(new File("stopwords.txt"));
+        Scanner textFile = new Scanner(new File("Text1.txt"));
 
-  public static void main(String[] args) {
-
-    try {
-    // Load the text files(stopwords.txt and Text1.txt)
-    Scanner stopWordsFile = new Scanner(new File("stopwords.txt"));
-    Scanner textFile = new Scanner(new File("Text1.txt"));
-
-    // Create a set for the stop words (a set as it doesn't allow duplicates)
-    Set<String> stopWords = new HashSet<String>();
-    // For each word in the file
-    while (stopWordsFile.hasNext()) {
-        stopWords.add(stopWordsFile.next().trim().toLowerCase());
-    }
-
-    // Splits strings and stores each word into a list
-    ArrayList<String> words = new ArrayList<String>();
-    while (textFile.hasNext()) {
-        words.add(textFile.next().trim().toLowerCase());
-    }
-
-    // Create an empty list (a list because it allows duplicates) 
-    ArrayList<String> listOfWords = new ArrayList<String>();
-
-    // Iterate over the array 
-    for(String word : words) {
-        // Converts current string index to lowercase
-        String toCompare = word.toLowerCase();
-        // If the word isn't a stop word, add to listOfWords list
-        if (!stopWords.contains(toCompare)) {
-            listOfWords.add(word);
+        // Create a set for the stop words (a set as it doesn't allow duplicates)
+        Set<String> stopWords = new HashSet<String>();
+        // For each word in the file
+        while (stopWordsFile.hasNext()) 
+        {
+            stopWords.add(stopWordsFile.next().trim().toLowerCase());
         }
-    }
 
-    stopWordsFile.close();
-    textFile.close();
+        // Splits strings and stores each word into a list
+        ArrayList<String> words = new ArrayList<String>();
+        while (textFile.hasNext()) 
+        {
+            words.add(textFile.next().trim().toLowerCase());
+        }
 
-    for (String str : listOfWords) {
-    // Print the final string (list of wrods)
-        System.out.print(str + " ");
-    }
-    } catch(FileNotFoundException e){
+        // Create an empty list (a list because it allows duplicates) 
+        ArrayList<String> listOfWords = new ArrayList<String>();
+
+        // Iterate over the array 
+        for(String word : words) 
+        {
+            // Converts current string index to lowercase
+            String toCompare = word.toLowerCase();
+            // If the word isn't a stop word, add to listOfWords list
+            if (!stopWords.contains(toCompare)) 
+            {
+                listOfWords.add(word);
+            }
+        }
+
+        stopWordsFile.close();
+        textFile.close();
+
+        for (String str : listOfWords) 
+        {
+        // Print the final string (list of wrods)
+            System.out.print(str + " ");
+        }
+    } 
+    catch(FileNotFoundException e)
+    {
         e.printStackTrace();
     }
-}
+  }
 }
