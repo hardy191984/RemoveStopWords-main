37def153bcee80b51854a8f37ce73769ac98562a
Hardik Patel <74027203+hardy191984@users.noreply.github.com>
2020-11-08 19:18:51 -0500

    Update StemmingWords.py

--- StemmingWords.py
+++ StemmingWords.py
@@ -1,4 +1,10 @@
-# load data
+# 1. import the nltk package
+import nltk
+
+# call the nltk downloader
+nltk.download()
+
+# 2. load data
 file = open("C:/Users/hpatel/Downloads/programming_assignment/Text1.txt", 'rt')
 text = file.read()
 file.close()
@@ -10,7 +16,7 @@ tokens = word_tokenize(text)
 # convert to lower case
 tokens = [w.lower() for w in tokens]
 
-# remove punctuation from each word
+# 3. remove punctuation from each word
 import string
 table = str.maketrans('', '', string.punctuation)
 stripped = [w.translate(table) for w in tokens]
@@ -18,19 +24,20 @@ stripped = [w.translate(table) for w in tokens]
 # remove remaining tokens that are not alphabetic
 words = [word for word in stripped if word.isalpha()]
 
-# filter out stop words
+# 4. filter out stop words
 file1 = open("C:/Users/hpatel/Downloads/programming_assignment/stopwords.txt", 'rt')
 text1 = file1.read()
 stop_words = set(text1.split())
 words = [w for w in words if w not in stop_words]
 print(words)
 
-# stemming of words
+# 5. stemming of words
 from nltk.stem.porter import PorterStemmer
 porter = PorterStemmer()
 stemmed = [porter.stem(word) for word in words]
 print(stemmed)
 
+# 6. Compute the frequency for each stem words into their room form and print out the 20 most words in descending order (do not include the stop words)
 import itertools
 all_words_nsw = list(itertools.chain(stemmed))
 import collections
